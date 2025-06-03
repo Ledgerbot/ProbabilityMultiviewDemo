7bcd9b00c77d1175a96cb5b471a78510ba8c7d8e
Michael Kutzer <kutzer@usna.edu>
2025-06-03 04:22:01 -0400

    Update VisualizeRigidBodyTransform.m

--- ProbabilityMultiviewDemo Example SCRIPTS/VisualizeRigidBodyTransform.m
+++ ProbabilityMultiviewDemo Example SCRIPTS/VisualizeRigidBodyTransform.m
@@ -12,11 +12,6 @@ axs = axes('Parent',fig);
 daspect(axs,[1 1 1]);
 hold(axs,'on');
 
-%% Adjust the axes properties to pretty-up the plot
-axis(axs,[-10,110,-10,50,-10,10]);
-grid(axs,'on');
-set(axs,'XTick',[-10:10:110]);
-
 %% Create coordinate frame representations
 h_1to0 = triad('Parent',axs,'AxisLabels',{'x_1','y_1','z_1'},...
     'Scale',12,'LineWidth',2);
@@ -26,3 +21,8 @@ h_2to1 = triad('Parent',h_1to0,'AxisLabels',{'x_2','y_2','z_2'},...
 %% Update pose of Frame 2 relative to Frame 1
 H_2to1 = Tx(96)*Ty(25)*Ry(-pi/2);   % Define mathematically
 set(h_2to1,'Matrix',H_2to1);        % Update visualization
+
+%% Adjust the axes properties to pretty-up the plot
+axis(axs,[-10,110,-10,50,-10,10]);
+grid(axs,'on');
+set(axs,'XTick',-10:10:110);
